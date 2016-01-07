643043edaf2dbdef7bab926b36f8c684d5196d64
Anil Allewar <anilallewar@Anils-MacBook-Pro.local>
2016-01-06 22:08:58 -0800

    Added address suggestion by overlaying table

--- UberProject/RideAcceptedDetailsViewController.swift
+++ UberProject/RideAcceptedDetailsViewController.swift
@@ -10,19 +10,50 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class RideAcceptedDetailsViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UITextFieldDelegate {
+class RideAcceptedDetailsViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource {
 
     var currentAcceptedRide:RideData?
     
     @IBOutlet var acceptedRideMapView: MKMapView!
+   
+    @IBOutlet var searchBar: UISearchBar!
     
-    @IBOutlet var destinationAddress: UITextField!
+    @IBOutlet var tripStartButton: UIButton!
     
     let locationManager = CLLocationManager()
+    
+    // Used for displaying the search results
+    var autoCompleteTableView:UITableView!
+    
+    var searchResults = [AddressSearch]()
+    
+    var selectedAddressIndex:Int = -1
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Load map with pickup address
+        self.setupInitialMap()
+        
+        // Handle the search bar click event
+        self.searchBar.delegate = self
+        
+        // Disable the start trip button
+        self.tripStartButton.enabled = false
+        self.tripStartButton.userInteractionEnabled = false
+        
+        self.autoCompleteTableView = UITableView(frame: CGRectMake(10, self.searchBar.center.y + self.searchBar.frame.height, self.searchBar.frame.width, 120), style: .Plain)
+        
+        // Initialize the auto-complete table
+        self.autoCompleteTableView.delegate = self
+        self.autoCompleteTableView.dataSource = self
+        self.autoCompleteTableView.scrollEnabled = true
+        self.autoCompleteTableView.hidden = true
+        
+        self.view.addSubview(self.autoCompleteTableView)
+    }
+    
+    private func setupInitialMap() {
         // Do any additional setup after loading the view.
         self.locationManager.delegate = self
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
@@ -61,6 +92,8 @@ class RideAcceptedDetailsViewController: UIViewController, MKMapViewDelegate, CL
     
     
     @IBAction func tripButtonClicked(sender: AnyObject) {
+        print("Trip start button clicked")
+        print("Selected index is: \(self.selectedAddressIndex) and the address is: \(self.searchResults[self.selectedAddressIndex].addressText) and the coordinates are: \(self.searchResults[self.selectedAddressIndex].addressCoordinate)")
     }
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -80,11 +113,13 @@ class RideAcceptedDetailsViewController: UIViewController, MKMapViewDelegate, CL
         
         directions.calculateDirectionsWithCompletionHandler { (response, error) -> Void in
             if error != nil {
-                print(error)
+                self.showAlert("Error calculating directions", message: (error?.localizedDescription)!)
             } else {
                 if let response = response {
                     if let calculatedRoute = response.routes[0] as? MKRoute {
                         self.acceptedRideMapView.addOverlay(calculatedRoute.polyline)
+                        // Stop updating locations once the initial overlay is laid
+                        self.locationManager.stopUpdatingLocation()
                     }
                 }
             }
@@ -103,7 +138,7 @@ class RideAcceptedDetailsViewController: UIViewController, MKMapViewDelegate, CL
         // If overlay is not polyline, then need to be handled differently
         return MKOverlayRenderer()
     }
-
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -111,5 +146,140 @@ class RideAcceptedDetailsViewController: UIViewController, MKMapViewDelegate, CL
         let viewController:RiderFeedTableTableViewController = segue.destinationViewController as! RiderFeedTableTableViewController
         
         viewController.acceptedRideIndex = -1
+        self.selectedAddressIndex = -1
     }
+    
+    func searchBarSearchButtonClicked(searchBarPassed: UISearchBar) {
+        
+        searchBarPassed.resignFirstResponder()
+        
+        let localSearchRequest:MKLocalSearchRequest = MKLocalSearchRequest()
+        localSearchRequest.naturalLanguageQuery = searchBarPassed.text
+        
+        let localSearch:MKLocalSearch = MKLocalSearch(request: localSearchRequest)
+        localSearch.startWithCompletionHandler { (searchResponse, error) -> Void in
+            // Initialize the address selection
+            self.selectedAddressIndex = -1
+            self.searchResults.removeAll()
+            
+            if error != nil {
+                self.showAlert("Error searching address", message: (error?.localizedDescription)!)
+            } else {
+                if searchResponse == nil {
+                    self.showAlert("No results", message: "No address found which matches the provided address")
+                } else {
+                    if let mapItems = searchResponse?.mapItems {
+                        self.populateAddressSearchResults(mapItems)
+                        self.autoCompleteTableView.hidden = false
+                    }
+                }
+                
+                // Reload the auto completion table
+                self.autoCompleteTableView.reloadData()
+            }
+        }
+    }
+    
+    private func populateAddressSearchResults (mapItems:[MKMapItem]) -> Void {
+        var messageText:String
+        
+        for mapItem in mapItems {
+            messageText = ""
+            
+            if let subStreet = mapItem.placemark.subThoroughfare {
+                messageText += subStreet
+            }
+            
+            if let street = mapItem.placemark.thoroughfare {
+                if messageText.characters.count > 0 {
+                    messageText += " " + street
+                } else {
+                    messageText = street
+                }
+            }
+            
+            if let locality = mapItem.placemark.locality {
+                if messageText.characters.count > 0 {
+                    messageText += ", " + locality
+                } else {
+                    messageText = locality
+                }
+            }
+            
+            if let administrativeArea = mapItem.placemark.administrativeArea {
+                if messageText.characters.count > 0 {
+                    messageText += ", " + administrativeArea
+                } else {
+                    messageText = administrativeArea
+                }
+            }
+            
+            if let postalCode = mapItem.placemark.postalCode {
+                if messageText.characters.count > 0 {
+                    messageText += ", " + postalCode
+                } else {
+                    messageText = postalCode
+                }
+            }
+            
+            let destinationCoordinates:CLLocationCoordinate2D = CLLocationCoordinate2DMake(mapItem.placemark.location!.coordinate.latitude , mapItem.placemark.location!.coordinate.longitude)
+            
+            let addressItem = AddressSearch(addressText:messageText, addressCoordinate:destinationCoordinates)
+            
+            self.searchResults.append(addressItem)
+
+        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.searchResults.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let autoCompleteCellIdentifier = "autoCell"
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier(autoCompleteCellIdentifier)
+        
+        if let tableCell = cell {
+            tableCell.textLabel?.text = searchResults[indexPath.row].addressText
+        } else {
+            cell = UITableViewCell(style: .Value1, reuseIdentifier: autoCompleteCellIdentifier)
+            cell!.textLabel?.text = searchResults[indexPath.row].addressText
+        }
+        
+        cell!.contentView.layer.borderColor = UIColor.redColor().CGColor
+        cell!.contentView.layer.borderWidth = 1.0
+        
+        return cell!
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if let selectedCell = tableView.cellForRowAtIndexPath(indexPath) {
+            self.searchBar.text = selectedCell.textLabel?.text
+            self.autoCompleteTableView.hidden = true
+            
+            // Set the selected address index
+            self.selectedAddressIndex = indexPath.row
+            
+            // Enable the start trip button
+            self.tripStartButton.enabled = true
+            self.tripStartButton.userInteractionEnabled = true
+
+        }
+    }
+    
+    private func showAlert (title:String, message:String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+}
+
+struct AddressSearch {
+    var addressText:String
+    var addressCoordinate:CLLocationCoordinate2D
 }
--- UberProject/RideData.swift
+++ UberProject/RideData.swift
@@ -13,10 +13,12 @@ class RideData {
     private var coordinates : CLLocationCoordinate2D!
     private var address : String = " "
     private var currentRideStatus: RideStatus = RideStatus.NEW
-    private var objectId:String!
-    private var riderUserId:String?
-    private var riderName:String?
-    private var riderImage:UIImage?
+    private var objectId : String!
+    private var riderUserId : String?
+    private var riderName : String?
+    private var riderImage : UIImage?
+    private var destinationAddress : String?
+    private var destinationCoordinates : CLLocationCoordinate2D?
     
     func getCoordinates() -> CLLocationCoordinate2D {
         return coordinates
@@ -72,6 +74,22 @@ class RideData {
     
     func setRiderImage(savedRiderImage:UIImage) -> Void {
         self.riderImage = savedRiderImage
+    }
+    
+    func getDestinationCoordinates() -> CLLocationCoordinate2D? {
+        return destinationCoordinates
+    }
+    
+    func setDestinationCoordinates(inputCoordinates:CLLocationCoordinate2D) -> Void {
+        self.destinationCoordinates = inputCoordinates
+    }
+    
+    func getDestinationAddress() -> String? {
+        return destinationAddress
+    }
+    
+    func setDestinationAddress(inputAddress:String) -> Void {
+        self.destinationAddress = inputAddress
     }
 }
 
